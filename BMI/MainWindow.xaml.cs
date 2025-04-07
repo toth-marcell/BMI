bdf9623674896b6e933bca450ae7e3e74c880f34
toth-marcell <toth.marcell.21i@egressy.info>
2025-04-07 13:37:33 +0200

    Add project files.

---
+++ BMI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BMI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void calculate(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    double mass = double.Parse(massField.Text);
+                    double height = double.Parse(heightField.Text);
+                    resultLabel.DataContext = mass / Math.Pow(height, 2);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is FormatException) MessageBox.Show("Számokat kell megadni!");
+                    else throw ex;
+                }
+            }
+        }
+    }
+}
---
+++ BMI/TextBoxWithPlaceholder.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BMI
+{
+    /// <summary>
+    /// Interaction logic for TextBoxWithPlaceholder.xaml
+    /// </summary>
+    public partial class TextBoxWithPlaceholder : TextBox
+    {
+        public string Placeholder
+        {
+            get => _placeholder;
+            set
+            {
+                _placeholder = value;
+                TextBox_LostFocus(null, null);
+            }
+        }
+        string _placeholder;
+        public bool PlaceholderVisible { get; set; }
+        public TextBoxWithPlaceholder()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PlaceholderVisible)
+            {
+                Text = "";
+                PlaceholderVisible = false;
+            }
+        }
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Text == "")
+            {
+                Text = Placeholder;
+                PlaceholderVisible = true;
+            }
+        }
+    }
+}
