55997ec9f8734b862d949d3a8bbfc878a64fe61f
toth-marcell <toth.marcell.21i@egressy.info>
2025-04-07 14:30:46 +0200

    add categories

--- BMI/MainWindow.xaml.cs
+++ BMI/MainWindow.xaml.cs
@@ -22,7 +22,9 @@ namespace BMI
                 {
                     double mass = double.Parse(massField.Text.Trim().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture);
                     double height = double.Parse(heightField.Text.Trim().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture);
-                    resultLabel.DataContext = Math.Round(mass / Math.Pow(height, 2), 2);
+                    double bmi = Math.Round(mass / Math.Pow(height, 2), 2);
+                    resultLabel.DataContext = bmi;
+                    categoryLabel.DataContext = BMICategory(bmi);
                 }
                 catch (Exception ex)
                 {
@@ -30,6 +32,15 @@ namespace BMI
                     else throw ex;
                 }
             }
+        }
+        string BMICategory(double bmi)
+        {
+            if (bmi < 18.5) return "sovány";
+            if (bmi < 25) return "normál";
+            if (bmi < 30) return "kicsit dagadt";
+            if (bmi < 35) return "egészen dagadt";
+            if (bmi < 35) return "nagyon dagadt";
+            return "teljesen dagadt";
         }
     }
 }
